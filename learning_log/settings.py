487a8ae3dcc648ae8967dd8997ec78d0157f4fa5
savelez <stephanievelez1986@gmail.com>
2024-01-22 12:07:38 -0600

    secure proxy ssl

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -173,10 +173,10 @@ BOOTSTRAP3 = {
 #         'default': dj_database_url.config(default='postgres://localhost')
 #     }
 
-    #Honor the 'X-Forwarded-Proto' header for request.is_secure().
-    # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    #
-    # #Allow all host headers.
+#Honor the 'X-Forwarded-Proto' header for request.is_secure().
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    
+#Allow all host headers.
 ALLOWED_HOSTS = ['*']
 
     #Static asset configuration.
