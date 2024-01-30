0e0048c8d121d542f0c8e0e03cdbc426c9d6dd97
savelez <stephanievelez1986@gmail.com>
2024-01-30 09:42:56 -0600

    changed key

--- alzheimerModel/model/neptune.py
+++ alzheimerModel/model/neptune.py
@@ -4,7 +4,7 @@ import os
 
 model = neptune.init_model(
     name="Prediction model",
-    key="MOD", 
+    key="AL-MOD", 
     project="stephanievelez1986/alzheimer-model", 
     api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI3NmJhZDY5Mi0wYjJlLTQ0YjItYmEwOS0yZjM2ODhkYjE0YmEifQ==", # your credentials
 )
